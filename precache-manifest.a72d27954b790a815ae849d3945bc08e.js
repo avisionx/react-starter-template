76d4534c35399d726acc38c9c26df6370b066eb8
avisionx <avi17223@iiitd.ac.in>
2020-10-04 08:13:11 +0000

    Deploying to gh-pages from  @ 27f3e4a24dd346145202894812b0bf4036b3fc71 🚀

---
+++ precache-manifest.a72d27954b790a815ae849d3945bc08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f965f4d709edc594f59ff22cd3ebba33",
+    "url": "/github-readme-cards/index.html"
+  },
+  {
+    "revision": "178e123251de524cf41e",
+    "url": "/github-readme-cards/static/css/main.c5bd1360.chunk.css"
+  },
+  {
+    "revision": "c55f8fa7098fdf60f85b",
+    "url": "/github-readme-cards/static/js/2.87117e50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-readme-cards/static/js/2.87117e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178e123251de524cf41e",
+    "url": "/github-readme-cards/static/js/main.57d209b9.chunk.js"
+  },
+  {
+    "revision": "7e0136e11a41c4a4b4f3",
+    "url": "/github-readme-cards/static/js/runtime-main.c417c8e0.js"
+  }
+]);
